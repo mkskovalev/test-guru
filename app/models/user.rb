f335395435fd6940e74d1ed3a5695949d1e3c198
mkskovalev <maxonemore@ya.ru>
2022-07-14 14:41:41 +0700

    fix: new logic in seens.rb and fixed def completed_tests_by_level in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   has_many :completed_tests
 
   def completed_tests_by_level(level)
-    CompletedTest.joins('INNER JOIN users ON completed_tests.user_id = users.id
-                         INNER JOIN tests ON completed_tests.test_id = tests.id')
-                         .where(tests: {level: level})
-                         .where(users: {id: self.id}).pluck(:title)
+    Test.where(level: level)
+        .joins('INNER JOIN completed_tests ON tests.id = completed_tests.test_id')
+        .where(completed_tests: {user_id: self.id})
   end
 end
