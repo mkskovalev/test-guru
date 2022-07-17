0f4a7aac01ac0139a076f82b1af93c60b37e9f8d
mkskovalev <maxonemore@ya.ru>
2022-07-17 23:36:53 +0700

    fix: remove answers count validation from Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,6 @@
 class Question < ApplicationRecord
   validates :body, presence: true
-  validate :validate_answers_count
 
   belongs_to :test
   has_many :answers
-
-  def validate_answers_count
-    errors.add(:answers_count, "it should be 1-4 answers for question") if (0..4).exclude?(self.answers.count)
-  end
 end
