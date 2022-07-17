d972974032dfa31ac7648c800d5e8165dc340ab2
mkskovalev <maxonemore@ya.ru>
2022-07-17 23:08:49 +0700

    fix: new validations for Question and Answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,6 +6,6 @@ class Question < ApplicationRecord
   has_many :answers
 
   def validate_answers_count
-    errors.add(:answers_count, "it should be 1-4 answers for question") if (1..4).exclude?(self.answers.count)
+    errors.add(:answers_count, "it should be 1-4 answers for question") if (0..4).exclude?(self.answers.count)
   end
 end
