fb1f6fe7941a11de25ddc627ef9f4936891002b8
mkskovalev <maxonemore@ya.ru>
2022-07-21 09:21:01 +0700

    fix: move Back link from Edit & New to _form, add except in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :tests do
-    resources :questions, shallow: true, only: [:show, :new, :create, :edit, :update, :destroy]
+    resources :questions, shallow: true, except: [:index]
   end
 end
