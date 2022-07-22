7b919b487979a02766e04c04904e7b51202fb421
mkskovalev <maxonemore@ya.ru>
2022-07-22 18:46:50 +0700

    feat: add some logic

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,8 +8,8 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: :User
   has_many :questions
-  has_many :tests_users
-  has_many :users, through: :tests_users
+  has_many :test_passages
+  has_many :users, through: :test_passages
 
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
   validates :name, :email, :password, presence: true
 
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :test_passages
+  has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: :Test, foreign_key: :user_id
 
   def tests_by_level(level)
     self.tests.where(level: level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
