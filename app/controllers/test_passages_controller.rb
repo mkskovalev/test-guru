ae69f5f8d289e992541eabf7526f48bbbe445f71
mkskovalev <maxonemore@ya.ru>
2022-07-22 21:47:03 +0700

    fix: add some fixes, add logic in TestPassage

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,14 +1,26 @@
 class TestPassagesController < ApplicationController
 
-  before_action :set_test_pasage, only: %i[show update result]
+  before_action :set_test_passage, only: %i[show update result]
 
   def show
+    unless @test_passage.comleted?
+      @completed_questions = @test_passage.question_count
+    end
   end
 
   def result
+    questions_count = @test_passage.test.questions.count.to_f
+   @result = ((@test_passage.correct_questions / questions_count) * 100).round(0)
   end
 
   def update
+    @test_passage.accept!(params[:answer_ids])
+
+    if @test_passage.comleted?
+      redirect_to result_test_passage_path(@test_passage)
+    else
+      render :show
+    end
   end
 
   private
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,9 +5,38 @@ class TestPassage < ApplicationRecord
 
   before_validation :before_validation_set_first_question, on: :create
 
+  def comleted?
+    current_question.nil?
+  end
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+
+    self.current_question = next_question
+    save!
+  end
+
+  def question_count
+    self.test.questions.where("id <= ?", self.current_question.id).count
+  end
+
   private
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
