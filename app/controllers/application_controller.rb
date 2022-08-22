1c2416525a85de337101e4339f6956c8cc2e7b84
mkskovalev <maxonemore@ya.ru>
2022-08-23 00:45:36 +0700

    fix: def admin? and resource instead of current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    flash[:notice] = "Привет, #{current_user.first_name}!"
-    current_user.is_a?(Admin) ? admin_tests_path : (stored_location_for(resource) || root_path)
+    flash[:notice] = "Привет, #{resource.first_name}!"
+    resource.admin? ? admin_tests_path : (stored_location_for(resource) || root_path)
   end
 end
