692a76557603186af315273439f73eab436bf568
mkskovalev <maxonemore@ya.ru>
2022-09-05 13:32:27 +0700

    fix: optimize code in password_confirmation.js

--- app/javascript/packs/password_confirmation.js
+++ app/javascript/packs/password_confirmation.js
@@ -2,18 +2,27 @@ document.addEventListener('turbolinks:load', function() {
 
   var passwordConfirmation = document.getElementById('user_password_confirmation')
 
-  if (passwordConfirmation) {
-    passwordConfirmation.addEventListener('input', function() {
-      if (!this.value) {
-        document.querySelector('.octicon-check-circle').classList.add('hide')
-        document.querySelector('.octicon-alert').classList.add('hide')
-      } else if (document.getElementById('user_password').value == this.value) {
-        document.querySelector('.octicon-check-circle').classList.remove('hide')
-        document.querySelector('.octicon-alert').classList.add('hide')
-      } else {
-        document.querySelector('.octicon-check-circle').classList.add('hide')
-        document.querySelector('.octicon-alert').classList.remove('hide')
-      }
-    })
-  }
+  if (passwordConfirmation) { passwordConfirmation.addEventListener('input', checkPassword) }
+
 })
+
+function checkPassword() {
+
+  var success = document.querySelector('.octicon-check-circle')
+  var failure = document.querySelector('.octicon-alert')
+
+  if (!this.value) {
+    success.classList.add('hide')
+    failure.classList.add('hide')
+    return
+  }
+
+  if (document.getElementById('user_password').value == this.value) {
+    success.classList.remove('hide')
+    failure.classList.add('hide')
+  } else {
+    success.classList.add('hide')
+    failure.classList.remove('hide')
+  }
+}
+
