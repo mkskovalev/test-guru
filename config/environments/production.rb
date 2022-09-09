7f1d21319e67af75d55b111a7571cde2b6362890
mkskovalev <maxonemore@ya.ru>
2022-09-09 15:07:59 +0700

    add smtp settings for mail.ru

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'limitless-reef-19631.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    adress: 'ssl://smtp.mail.ru',
+    adress: 'smtp.mail.ru',
     port: 465,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
