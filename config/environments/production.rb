aab0c6430193e823284196d0c4324e7d6ffb36e3
mkskovalev <maxonemore@ya.ru>
2022-09-09 15:29:27 +0700

    add smtp settings for gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'limitless-reef-19631.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    adress: 'smtp.mail.ru',
-    port: 465,
+    adress: 'smtp.gmail.com',
+    port: 587,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
