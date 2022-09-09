1e0ef6e960f44698c59538f0c66e4f1ecf949cfe
mkskovalev <maxonemore@ya.ru>
2022-09-09 15:01:32 +0700

    add smtp settings

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_08_25_115658) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false
