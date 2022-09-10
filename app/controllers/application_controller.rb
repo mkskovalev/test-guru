1cfc3c7d48e66ee95bce701e8d86661db609beba
mkskovalev <maxonemore@ya.ru>
2022-09-10 14:07:40 +0700

    add recording to db first_name & last_name in sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
   before_action :authenticate_user!
 
@@ -20,5 +21,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     flash[:info] = "Привет, #{resource.first_name}!"
     resource.admin? ? admin_tests_path : (stored_location_for(resource) || root_path)
+  end
+
+  def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
