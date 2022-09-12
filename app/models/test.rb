2dc1c5537066229a98e8aca413be62470bfb3624
mkskovalev <maxonemore@ya.ru>
2022-09-12 18:43:10 +0700

    fix: add activate func to tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -15,8 +15,13 @@ class Test < ApplicationRecord
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
   scope :joins_category, ->(name) { joins(:category).where(categories: {title: name}) }
+  scope :active, -> { where(active: true) }
 
   def self.tests_by_category(name)
     self.joins_category(name).order(title: :asc).pluck(:title)
+  end
+
+  def activated?
+    self.active == true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_115658) do
+ActiveRecord::Schema.define(version: 2022_09_12_110416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2022_08_25_115658) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id", null: false
     t.integer "user_id"
+    t.boolean "active", default: false
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
     t.index ["user_id"], name: "index_tests_on_user_id"
