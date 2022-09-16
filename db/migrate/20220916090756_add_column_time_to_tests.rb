1ead7b510be630c8f1830e4a06e3b99375161904
mkskovalev <maxonemore@ya.ru>
2022-09-16 17:48:04 +0700

    fix: add up & down in migration 20220916090756

--- db/migrate/20220916090756_add_column_time_to_tests.rb
+++ db/migrate/20220916090756_add_column_time_to_tests.rb
@@ -1,6 +1,15 @@
 class AddColumnTimeToTests < ActiveRecord::Migration[6.1]
-  def change
+  def up
     remove_columns :tests, :with_timer
     drop_table :timers
+  end
+
+  def down
+    create_table :timers do |t|
+      t.references :test_passage, null: false, foreign_key: true
+      t.integer :end_time, null: false
+      t.timestamps
+    end
+    add_column :tests, :with_timer, :boolean, default: false
   end
 end
