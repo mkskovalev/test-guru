e34a62d9f7cc0fd36aa0aa51f09299af019d6d4d
mkskovalev <maxonemore@ya.ru>
2022-09-16 17:00:26 +0700

    fix: delete has_one :timer from test_passage model

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -2,7 +2,6 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
-  has_one :timer
 
   before_validation :before_validation_set_first_question, on: :create
   before_update :before_update_set_next_question
