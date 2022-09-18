e10ea82c41982e1ccfb8960cf5e0866a452169a8
mkskovalev <maxonemore@ya.ru>
2022-09-18 16:45:06 +0700

    tidied up migrations

--- db/migrate/20220915205412_create_timers.rb
+++
@@ -1,9 +0,0 @@
-class CreateTimers < ActiveRecord::Migration[6.1]
-  def change
-    create_table :timers do |t|
-      t.references :test_passage, null: false, foreign_key: true
-      t.integer :end_time, null: false
-      t.timestamps
-    end
-  end
-end
